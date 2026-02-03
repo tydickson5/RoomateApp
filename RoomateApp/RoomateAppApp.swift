da5178d46fa6a300477766f55c46d2ae04e1101b
canvassed <tydickson255@gmail.com>
2026-02-03 18:05:27 -0500

    Added email auth, google auth, and apple auth(potentially not working) New home page New login page New director page based on login status

--- RoomateApp/RoomateAppApp.swift
+++ RoomateApp/RoomateAppApp.swift
@@ -1,22 +1,29 @@
 import SwiftUI
+import GoogleSignIn
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+      func application(_ application: UIApplication,
+                       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            FirebaseApp.configure()
+            return true
+      }
 }
 
 @main
 struct RoomateAppApp: App {
-  // Register the delegate
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    // Register the delegate
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+        
+      @StateObject private var authManager = AuthManager()
 
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-    }
-  }
+      var body: some Scene {
+            WindowGroup {
+                ContentView()
+                    .environmentObject(authManager)
+                    .onOpenURL { url in
+                        GIDSignIn.sharedInstance.handle(url)
+                    }
+            }
+      }
 }
